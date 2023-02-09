df74c06e44396c00023c5715bfce8e6ed58e6d8d
ComunicacionItecnos <90643612+ComunicacionItecnos@users.noreply.github.com>
2023-02-09 17:08:20 -0600

    add files

---
+++ index.js
@@ -0,0 +1,18 @@
+import React from "react";
+import { NativeModules } from "react-native";
+const { RNCustomCode } = NativeModules;
+
+import { initialize } from "@blocklienterprise/blockli";
+import config from "@src/build_config.json";
+
+export const applyCustomCode = async (externalCodeSetup) => {
+  const blockli_config = {
+    license: "HC3YFW9EVBR4BB1", // Enter your 15 digit Blockli App Kit license key here. See your account dashboard at https://blockli.dev/dashboard
+    app_id: config.app_id,
+    code: externalCodeSetup,
+    website: "https://fctr.app", // Enter your app domain here with NO trailing slash. 
+    // NOTE: If you have a staging site, please use a different Git branch for your test app and COPY this entire code to the test branch and enter the staging site domain. 
+  };
+
+  await initialize(blockli_config);
+};
